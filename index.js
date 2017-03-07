96c56765f87df66a6f2f21b04aff5dfb67521290
Dmitriy Nevzorov <jimmy.lugat@gmail.com>
2017-03-07 20:22:11 +0300

    register filter only in production

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 'use strict';
 
-// Object.assign()
-
-// console.log(hexo.config);
-
-var gzip = hexo.config.gzip || {}; 
-
+var gzip = hexo.config.gzip || {};
 var priority = gzip.priority || 10000;
 
-hexo.extend.filter.register('after_generate', require('./lib/gzip'), priority);
+if (process.env.NODE_ENV !== 'development') {
+  hexo.extend.filter.register('after_generate', require('./lib/gzip'), priority);
+}
